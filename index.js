07731314440fe3a91d52156a125855dd6c05444c
ezequieleandi <eandiezequiel@gmail.com>
2023-02-09 09:57:16 -0300

    Desafio con express

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 const path = require("path");
 const ManagerProduct = require("./managerProducts");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const rutaBase = path.join(`${__dirname}/db.json`);
+const Manager = new ManagerProduct(rutaBase);
 
-const functionsProduct = async () => {
-    try {
-        const rutaBase = path.join(`${__dirname}/db.json`);
-        const Manager = new ManagerProduct(rutaBase);
-        //Se muestra el array inicial de productos
-        console.log("Array inicial")
-        const listaProducts = await Manager.getProducts();
-        console.log(listaProducts);
-        //Se crea nuevo producto y se muestra array nuevo
-        console.log("Creando producto nuevo y muestra de array")
-        const createProduct = {
-            title: "zapatillas convers",
-            description: "Zapatillas 2023",
-            price: 12500,
-            stock: 15,
-        };
-        await Manager.addProduct(createProduct);
-        const listaNewProducts = await Manager.getProducts();
-        console.log(listaNewProducts);
-        //Buscando un producto especifico
-        console.log("Buscando producto por ID")
-        const productById = await Manager.getProductById(2)
-        console.log(productById)
-        //Eliminando un producto y mostrando array
-        console.log("Eliminando un producto y mostrando array")
-        await Manager.deleteProduct(2)
-        const productsDelete = await Manager.getProducts();
-        console.log(productsDelete);
-        //modificando producto y mostrando array
-        console.log("modificando producto")
-        const newInfo = {
-            talle: "XL"
-        }
-        await Manager.upDateProduct(3, newInfo)
-        const editProduct = await Manager.getProducts();
-        console.log(editProduct);
-    } catch (error) {
-        console.log(error);
+app.get('/products', async (req, res) => {
+    const limit = Number(req.query.limit)
+    const listaProducts = await Manager.getProducts();
+    if (limit <= listaProducts.length) {
+        return res.json(listaProducts.slice(0, limit))
+    } else if(limit > (listaProducts.length - 1)) {
+        return res.json({
+            mensaje: "No existe esa cantidad de productos",
+        })
+    }else{
+        return res.json({
+            mensaje: "Lista de productos",
+            productos: listaProducts
+        })
     }
-};
+})
+app.get('/productos/:id', async (req, res) => {
+	const id = Number(req.params.id);
+	const product = await Manager.getProductById(id);
+    if(!product){
+        return res.json({ mensaje: 'Producto no encontrado. Corrobore el dato ingresado' });
+    }else{
+        res.json({producto: product});
+    }
+});
 
-functionsProduct();
+const PORT = 8082;
+app.listen(PORT, () => {
+    console.log("escuchando al puerto")
+});
